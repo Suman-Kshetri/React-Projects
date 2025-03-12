dc9accca6d89d5bc48b44634bafbfc087c388eed
Suman-Kshetri <ksuman20601028@gmail.com>
2025-03-12 17:45:38 +0545

    login form using tanstark router, shadcn, rect-hook-form, zord

---
+++ Typescript/login-page/src/App.tsx
@@ -0,0 +1,11 @@
+import StatefulLoginForm from "./components/StatefulLoginForm";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <StatefulLoginForm />
+    </div>
+  );
+};
+
+export default App;
